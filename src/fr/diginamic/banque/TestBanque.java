79429b3a4907cadb02c91514341ac8573bf5e39b
Cooljeb <joseph.brou@gmail.com>
2024-10-04 17:51:47 +0200

    HERITAGE ET CONSTRUCTEURS

--- src/fr/diginamic/banque/TestBanque.java
+++ src/fr/diginamic/banque/TestBanque.java
@@ -1,12 +1,22 @@
 package fr.diginamic.banque;
 
 import fr.diginamic.banque.entites.Compte;
+import fr.diginamic.banque.entites.CompteTaux;
 
 public class TestBanque {
 
     public static void main(String[] args) {
-        Compte cpt1 = new Compte(1,3000);
-        System.out.println(cpt1);
+        Compte tabCompte[] = new Compte[2]; // tableau de compte
+        Compte cpt1 = new Compte(1,3000); // compte courant
+        CompteTaux cpt2 =  new CompteTaux(2,2563,5.50);// compte + taux
+        //affectation des valeurs au tableau de comtpes
+        tabCompte[0] = cpt1;
+        tabCompte[1] = cpt2;
+
+        // lecture du tableau avec la représentation du de chaque objet
+        for (int i = 0; i < tabCompte.length ; i++) {
+            System.out.println(tabCompte[i].toString());
+        }
     }
 
 }
---
+++ src/fr/diginamic/banque/entites/CompteTaux.java
@@ -0,0 +1,32 @@
+package fr.diginamic.banque.entites;
+
+public class CompteTaux extends Compte {
+
+    /**
+     * Constructeur de compte
+     *
+     * @param numCompte   alias numéro de compte
+     * @param soldeCompte alias solde du compte
+     * @param taux rémunération
+     */
+
+    private double taux = 0.00f;
+    public CompteTaux(int numCompte, int soldeCompte, double taux) {
+        super(numCompte, soldeCompte);
+        this.taux=taux;
+    }
+
+    public double getTaux() {
+        return taux;
+    }
+
+    public void setTaux(float taux) {
+        this.taux = taux;
+    }
+
+    @Override public String toString() {
+
+        String infosCompte = super.toString();
+        return "CompteTaux{ "+infosCompte+" + taux "  + taux +" % }";
+    }
+}
