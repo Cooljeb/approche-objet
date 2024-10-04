f60c4354bb683ae66b9e8b34f4be2ea0fdcb99b0
Cooljeb <joseph.brou@gmail.com>
2024-10-04 16:34:56 +0200

    SP N°4 v2

--- src/fr/entites/utils/TestMethodeStatic.java
+++ src/fr/entites/utils/TestMethodeStatic.java
@@ -5,7 +5,7 @@ public class TestMethodeStatic {
 
         String chaine = "12";
         int convertionInt = Integer.parseInt(chaine);
-        int a = 50, b=12;
+        int a = 50, b=convertionInt;
         System.out.println(Integer.max(a,b));
     }
 }
