ce8f869a7704ec1e5cb68d0047e286540fe5933c
Cooljeb <joseph.brou@gmail.com>
2024-10-04 15:12:34 +0200

    SP N°4

---
+++ src/fr/entites/utils/TestMethodeStatic.java
@@ -0,0 +1,11 @@
+package fr.entites.utils;
+
+public class TestMethodeStatic {
+    public static void main(String[] args) {
+
+        String chaine = "12";
+        int convertionInt = Integer.parseInt(chaine);
+        int a = 50, b=12;
+        System.out.println(Integer.max(a,b));
+    }
+}
